35600676300395a6d43863c92010c9076fafb33d
Zisp0 <92941421+Zisp0@users.noreply.github.com>
2022-06-08 10:37:32 -0500

    Add files via upload

---
+++ Interprete/static/Interprete/js/logicaVista.js
@@ -0,0 +1,90 @@
+$(function () {
+    $("#btnVerificar").click(function (e) {
+        let codigo = $("#codigoDigitado").val().trim();
+        $("#textico").empty()
+        if (codigo != "") {
+            $.ajax({
+                url: "validar/",
+                method: "GET",
+                dataType: "json",
+                data:{
+                    'codigo': codigo
+                },
+                success: function (respuesta) {
+                    $("#textico").text(respuesta.resultado.replaceAll("\n", "<br>"))
+                    console.log(respuesta)
+                }
+            });
+        } else {
+            campoVacio()
+        }
+    });
+
+    $("#btnEjecutar").click(function (e) {
+        let codigo = $("#codigoDigitado").val().trim();
+        $("#textico").empty()
+        if (codigo != "") {
+            $.ajax({
+                url: "ejecutar/",
+                method: "GET",
+                dataType: "json",
+                data:{
+                    'codigo': codigo
+                },
+                success: function (respuesta) {
+                    $("#textico").append(respuesta.resultado.replaceAll("\n", "<br>"))
+                    console.log(respuesta)
+                }
+            });
+        } else {
+            campoVacio()
+        }
+    });
+});
+
+function deselect() {
+    $('.form-select').attr("disabled", true);
+    $('.form-select').attr("disabled", false);
+}
+
+//Alertas
+
+function campoVacio() {
+    Swal.fire({
+        title: atencion,
+        text: txAreaVacio,
+        icon: 'warning'
+    })
+
+    speak(txAreaVacio)
+}
+
+function validarOk() {
+    Swal.fire({
+        title: atencion,
+        text: txvalidarOk,
+        icon: 'success'
+    })
+
+    speak(txvalidarOk)
+}
+
+function validarMal() {
+    Swal.fire({
+        title: atencion,
+        text: txvalidarMal,
+        icon: 'error'
+    })
+
+    speak(txvalidarMal)
+}
+
+function ejecutarOk() {
+    Swal.fire({
+        title: atencion,
+        text: txEjecutar,
+        icon: 'success'
+    })
+
+    speak(txEjecutar)
+}
